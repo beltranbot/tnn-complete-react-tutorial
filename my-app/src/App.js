9cf70cfe1286bb5422e8e9fa3d36e5295602af70
hx <beltranbot@gmail.com>
2018-10-04 21:38:31 -0500

    # 13 - props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,21 @@ import Ninjas from './Ninjas'
 
 class App extends Component {
     render () {
+        
         return (
             <div className="App">
                 <h1>My first React App!</h1>
                 <p>Welcome :)</p>
-                <Ninjas />
+                <Ninjas
+                    name="Sasuke"
+                    age="25"
+                    belt="black"
+                />
+                <Ninjas
+                    name="Yoshi"
+                    age="30"
+                    belt="yellow"
+                />
             </div>
         )
     }
