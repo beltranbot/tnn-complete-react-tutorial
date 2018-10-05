faf35b69af1cbc6292c300de07e7462c514dc554
hx <beltranbot@gmail.com>
2018-10-04 21:34:42 -0500

    # 12 - Nesting Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Ninjas from './Ninjas'
+
 class App extends Component {
-    render() {
+    render () {
         return (
             <div className="App">
                 <h1>My first React App!</h1>
                 <p>Welcome :)</p>
+                <Ninjas />
             </div>
         )
     }
