b87c60bb404fe9f7718fbe97a242c454d0cc40bd
hx <beltranbot@gmail.com>
2018-10-06 16:38:19 -0500

    # 31 - route parameters (part 2)

--- poketimes/src/components/About.js
+++ poketimes/src/components/About.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Rainbow from '../hoc/Rainbow'
 
 const About = () => {
-    return (
-        <div className="container">
-            <h4 className="center">About</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus saepe similique maxime perspiciatis nihil aliquid cupiditate adipisci. Blanditiis, sit quaerat velit possimus doloribus illo mollitia, voluptatum ea voluptatibus exercitationem dolor.</p>
-        </div>
-    )
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus saepe similique maxime perspiciatis nihil aliquid cupiditate adipisci. Blanditiis, sit quaerat velit possimus doloribus illo mollitia, voluptatum ea voluptatibus exercitationem dolor.</p>
+    </div>
+  )
 }
 
 export default Rainbow(About)
