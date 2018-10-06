4a473e403abc95ec91e78625de9bac808939b2da
hx <beltranbot@gmail.com>
2018-10-06 18:22:44 -0500

    # 40 - mapping state to props

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,27 +1,16 @@
 // to use lifecycle hoooks
 // a component needs to be a class component
 import React, {Component} from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import {Link} from 'react-router-dom'
 import Pokeball from '../pokeball.png'
+import {connect} from 'react-redux'
 
 class Home extends Component {
 
-    state = {
-      posts: []
-    }
-
-    componentDidMount () {
-      axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res => {
-          this.setState({
-            posts: res.data.slice(0, 10)
-          })
-        })
-    }
-
     render () {
-      const {posts} = this.state
+      console.log(this.props.posts)
+      const {posts} = this.props
 
       const postList = posts.length ? (
         posts.map(post => {
@@ -50,4 +39,10 @@ class Home extends Component {
     }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps)(Home)
--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-  posts: []
+  posts: [
+    {id: '1', title: 'Squirtle laid an egg', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, pariatur itaque molestias similique debitis incidunt eum magni reprehenderit! Laborum qui sed beatae facere odit, consectetur numquam eaque sequi illo inventore.'},
+    {id: '2', title: 'Charmander laid an egg', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, pariatur itaque molestias similique debitis incidunt eum magni reprehenderit! Laborum qui sed beatae facere odit, consectetur numquam eaque sequi illo inventore.'},
+    {id: '3', title: 'a Helix Fossil was an egg', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, pariatur itaque molestias similique debitis incidunt eum magni reprehenderit! Laborum qui sed beatae facere odit, consectetur numquam eaque sequi illo inventore.'}
+  ]
 }
 
 const rootReducer = (state = initState, action) => {
