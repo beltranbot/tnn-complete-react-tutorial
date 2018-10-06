f59e1b491b7a8c95ea3d3e1df4fefcfb19d173c2
hx <beltranbot@gmail.com>
2018-10-06 15:38:33 -0500

    # 29 - Using Axios

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,45 @@
-import React from 'react'
+// to use lifecycle hoooks
+// a component needs to be a class component
+import React, {Component} from 'react'
+import axios from 'axios'
 
-const Home = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus saepe similique maxime perspiciatis nihil aliquid cupiditate adipisci. Blanditiis, sit quaerat velit possimus doloribus illo mollitia, voluptatum ea voluptatibus exercitationem dolor.</p>
-        </div>
-    )
+
+class Home extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                this.setState({
+                    posts: res.data.slice(0, 10)
+                })
+            })
+    }
+    render () {
+        const {posts} = this.state
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="posts card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+
+        ) : (
+            <div className="center">No Posts Yet</div>
+        )
+        return (
+            <div className="container">
+                <h4 className="center">Home</h4>
+                {postList}
+            </div>
+        )
+    }
 }
 
 export default Home
